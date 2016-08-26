bc137ff423d6a2f5a8c80ff89592b80972519298
Lucas Thompson <dev@lucas.im>
2016-08-26 08:49:18 +0100

    Bring in process implementation for ZC from the C++ / Vamp Example Plugin.

--- src/ZeroCrossings.ts
+++ src/ZeroCrossings.ts
@@ -1,17 +1,39 @@
 /**
- * Created by lucas on 26/08/2016.
+ * Created by lucas on 25/08/2016.
  */
 import {FeatureExtractor} from "./FeatureExtractor";
 import {Feature} from "./Feature";
 
 export class ZeroCrossings implements FeatureExtractor {
+    private previousSample: number;
 
     constructor() {
+        this.previousSample = 0;
     }
 
     process(block: Float32Array): Feature[] {
+        let prev: number = this.previousSample;
+        let count: number = 0;
         let returnFeatures: Feature[] = [];
 
-        return [];
+        block.forEach((sample) => {
+            let crossing: boolean = false;
+
+            if (sample <= 0.0) {
+                if (prev > 0.0) crossing = true;
+            } else if (sample > 0.0) {
+                if (prev <= 0.0) crossing = true;
+            }
+
+            if (crossing) {
+                ++count;
+            }
+
+            prev = sample;
+        });
+
+        this.previousSample = prev;
+        returnFeatures.push({values: [count]});
+        return returnFeatures;
     }
 }
