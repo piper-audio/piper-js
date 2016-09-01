cffe93352e09cbb8e3121a70ffdb34998ed22194
Lucas Thompson <dev@lucas.im>
2016-09-01 11:10:36 +0100

    Refactor async stuff slightly.

--- src/EmscriptenPluginServer.ts
+++ src/EmscriptenPluginServer.ts
@@ -10,7 +10,7 @@ import {
     Response, Request
 } from './PluginServer';
 import {Feature} from "./Feature";
-import ExampleModule = require('../ext/ExampleModule');
+import VamPipeServer = require('../ext/ExampleModule');
 import {Allocator, EmscriptenModule} from "./Emscripten";
 
 export class EmscriptenPluginServer implements PluginServer {
@@ -19,28 +19,27 @@ export class EmscriptenPluginServer implements PluginServer {
     private freeJson: (ptr: number) => void;
 
     constructor() {
-        this.server = ExampleModule();
+        this.server = VamPipeServer();
         this.doRequest = this.server.cwrap('vampipeRequestJson', 'number', ['number']) as (ptr: number) => number;
         this.freeJson =  this.server.cwrap('vampipeFreeJson', 'void', ['number']) as (ptr: number) => void;
     }
 
-    private request(request: Request): Response { // TODO this should be a Promise of a Request as this is the async work
-        const requestJson: string = JSON.stringify(request);
-        const requestJsonPtr: number = this.server.allocate(this.server.intArrayFromString(requestJson), 'i8', Allocator.ALLOC_NORMAL);
-        const responseJsonPtr: number = this.doRequest(requestJsonPtr);
-        this.server._free(requestJsonPtr);
-        var response: Response = JSON.parse(this.server.Pointer_stringify(responseJsonPtr));
-        this.freeJson(responseJsonPtr);
-        return response;
+    private request(request: Request): Promise<Response> {
+        return new Promise<Response>((resolve) => {
+            const requestJson: string = JSON.stringify(request);
+            const requestJsonPtr: number = this.server.allocate(this.server.intArrayFromString(requestJson), 'i8', Allocator.ALLOC_NORMAL);
+            const responseJsonPtr: number = this.doRequest(requestJsonPtr);
+            this.server._free(requestJsonPtr);
+            var response: Response = JSON.parse(this.server.Pointer_stringify(responseJsonPtr));
+            this.freeJson(responseJsonPtr);
+            resolve(response);
+        });
     }
 
     listPlugins(): Promise<StaticData[]> {
-        return new Promise<StaticData[]>((resolve, reject) => { // TODO .then chain of this.request
-            const response: Response = this.request({"type": "list"} as Response);
-            if (!response.success) reject(response.errorText);
-
-            const pluginList: StaticData[] = response.content.plugins; // TODO this isn't declared in any type
-            resolve(pluginList);
+        return this.request({"type": "list"} as Request).then((response) => {
+            if (!response.success) throw new Error(response.errorText);
+            return response.content.plugins as StaticData[];
         });
     }
 
