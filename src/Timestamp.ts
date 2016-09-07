91f030bc41e86dbba49813697eaaf6971ce72f80
Lucas Thompson <dev@lucas.im>
2016-09-07 15:58:22 +0100

    Manually bring in Chris' timestamp stuff from his fork. (cherry picked from commit b5c5169)

--- src/Timestamp.ts
+++ src/Timestamp.ts
@@ -5,3 +5,21 @@ export interface Timestamp {
     s: number,
     n: number
 }
+
+//!!! or a class
+//!!! + test (after RealTime tests elsewhere)
+export function makeTimestamp(seconds : number) : Timestamp {
+    if (seconds >= 0.0) {
+        return {
+            s : Math.floor(seconds),
+            n : Math.floor((seconds - Math.floor(seconds)) * 1e9 + 0.5)
+        };
+    } else {
+        const { s, n } = makeTimestamp(-seconds);
+        return { s : -s, n : -n };
+    }
+}
+
+export function frame2timestamp(frame : number, rate : number) : Timestamp {
+    return makeTimestamp(frame / rate);
+}
