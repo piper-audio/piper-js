ba4bd547e02d5c6bd5a4aaaa297beb7f9f5e71ea
Lucas Thompson <dev@lucas.im>
2016-09-12 16:41:34 +0100

    Whitespace changes.

--- src/EmscriptenPluginServer.ts
+++ src/EmscriptenPluginServer.ts
@@ -40,7 +40,7 @@ export class EmscriptenPluginServer implements PluginServer {
     constructor() {
         this.server = VamPipeServer();
         this.doRequest = this.server.cwrap('vampipeRequestJson', 'number', ['number']) as (ptr: number) => number;
-        this.doProcess = this.server.cwrap('vampipeProcessRaw', 'number', ['number','number','number','number']) as (handle: number, bufs: number, sec: number, nsec: number) => number;
+        this.doProcess = this.server.cwrap('vampipeProcessRaw', 'number', ['number', 'number', 'number', 'number']) as (handle: number, bufs: number, sec: number, nsec: number) => number;
         this.freeJson = this.server.cwrap('vampipeFreeJson', 'void', ['number']) as (ptr: number) => void;
         this.timeAdjusters = new Map();
     }
@@ -49,17 +49,17 @@ export class EmscriptenPluginServer implements PluginServer {
 
         return new Promise<Response>((resolve, reject) => {
 
-	    const requestJson: string = JSON.stringify(request);
+            const requestJson: string = JSON.stringify(request);
             const requestJsonPtr: number = this.server.allocate(
-		this.server.intArrayFromString(requestJson), 'i8',
-		Allocator.ALLOC_NORMAL);
+                this.server.intArrayFromString(requestJson), 'i8',
+                Allocator.ALLOC_NORMAL);
 
             const responseJsonPtr: number = this.doRequest(requestJsonPtr);
 
             this.server._free(requestJsonPtr);
 
             var response: Response = JSON.parse(
-		this.server.Pointer_stringify(responseJsonPtr));
+                this.server.Pointer_stringify(responseJsonPtr));
 
             this.freeJson(responseJsonPtr);
 
@@ -106,10 +106,10 @@ export class EmscriptenPluginServer implements PluginServer {
             return features;
         });
     }
-    
+
     processBase64(request: ProcessRequest): Promise<FeatureSet> {
         const encoded = request.processInput.inputBuffers.map(channel => {
-            return { b64values: EmscriptenPluginServer.toBase64(channel.values) }
+            return {b64values: EmscriptenPluginServer.toBase64(channel.values)}
         });
         const encReq = {
             pluginHandle: request.pluginHandle,
@@ -118,13 +118,13 @@ export class EmscriptenPluginServer implements PluginServer {
                 inputBuffers: encoded
             }
         };
-        return this.request({type: 'process', content: encReq }).then((response) => {
+        return this.request({type: 'process', content: encReq}).then((response) => {
             let features: FeatureSet = EmscriptenPluginServer.responseToFeatureSet(response);
             this.adjustFeatureTimes(features);
             return features;
         });
     }
-    
+
     processRaw(request: ProcessRequest): Promise<FeatureSet> {
         return this.makeRawProcessCall(request).then((response) => {
             let features: FeatureSet = EmscriptenPluginServer.responseToFeatureSet(response);
@@ -150,7 +150,7 @@ export class EmscriptenPluginServer implements PluginServer {
             const bufsPtr = this.server._malloc(nchannels * 4);
             const bufs = new Uint32Array(
                 this.server.HEAPU8.buffer, bufsPtr, nchannels);
-            
+
             for (let i = 0; i < nchannels; ++i) {
                 const framesPtr = this.server._malloc(nframes * 4);
                 const frames = new Float32Array(
@@ -158,7 +158,7 @@ export class EmscriptenPluginServer implements PluginServer {
                 frames.set(request.processInput.inputBuffers[i].values);
                 bufs[i] = framesPtr;
             }
-                
+
             const responseJsonPtr: number = this.doProcess(
                 request.pluginHandle,
                 bufsPtr,
@@ -169,11 +169,11 @@ export class EmscriptenPluginServer implements PluginServer {
                 this.server._free(bufs[i]);
             }
             this.server._free(bufsPtr);
-            
+
             var response: Response = JSON.parse(
                 this.server.Pointer_stringify(responseJsonPtr));
             this.freeJson(responseJsonPtr);
-            
+
             if (!response.success) {
                 throw new Error(response.errorText);
             } else {
@@ -181,8 +181,8 @@ export class EmscriptenPluginServer implements PluginServer {
             }
         });
     }
-        
-    private static toBase64(values: Float32Array) : string {
+
+    private static toBase64(values: Float32Array): string {
         // We want a base-64 encoding of the raw memory backing the
         // typed array. We assume byte order will be the same when the
         // base-64 stuff is decoded, but I guess that might not be
@@ -205,7 +205,7 @@ export class EmscriptenPluginServer implements PluginServer {
     }
 
     private static convertWireFeature(wfeature: WireFeature): Feature {
-        let out : Feature = {};
+        let out: Feature = {};
         if (wfeature.timestamp != null) {
             out.timestamp = wfeature.timestamp;
         }
@@ -226,13 +226,13 @@ export class EmscriptenPluginServer implements PluginServer {
     private static convertWireFeatureList(wfeatures: WireFeature[]): Feature[] {
         return wfeatures.map(EmscriptenPluginServer.convertWireFeature);
     }
-    
+
     private static responseToFeatureSet(response: Response): FeatureSet {
         const features: FeatureSet = new Map();
         Object.keys(response.content).forEach(
             key => features.set(Number.parseInt(key),
-                                EmscriptenPluginServer.convertWireFeatureList(
-                                    response.content[key])));
+                EmscriptenPluginServer.convertWireFeatureList(
+                    response.content[key])));
         // TODO seems awkward and inefficient converting an object to a map
         return features;
     }
