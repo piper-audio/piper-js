775d6d7914fe72bd8aee33f7e640a486ad394cf1
Lucas Thompson <dev@lucas.im>
2016-09-17 20:24:17 +0100

    Refactor slightly, reducing duplicate code.

--- src/EmscriptenModuleRequestHandler.ts
+++ src/EmscriptenModuleRequestHandler.ts
@@ -4,6 +4,7 @@
 import {EmscriptenModule, Allocator} from "./Emscripten";
 import {Response, Request, ModuleRequestHandler, ProcessRequest} from "./ClientServer";
 
+type ResponsePointer = number;
 export class EmscriptenModuleRequestHandler implements ModuleRequestHandler {
 
     private server: EmscriptenModule;
@@ -19,73 +20,61 @@ export class EmscriptenModuleRequestHandler implements ModuleRequestHandler {
     }
 
     handle(request: Request): Promise<Response> {
-        if (request.type === "process") return this.processRaw(request.content as ProcessRequest);
-        return this.respond(request);
-    }
-
-    private respond(request: Request): Promise<Response> {
         return new Promise<Response>((resolve, reject) => {
-            const requestJson: string = JSON.stringify(request);
-            const requestJsonPtr: number = this.server.allocate(
-                this.server.intArrayFromString(requestJson), "i8",
-                Allocator.ALLOC_NORMAL);
-
-            const responseJsonPtr: number = this.doRequest(requestJsonPtr);
-
-            this.server._free(requestJsonPtr);
+            const responseJson: ResponsePointer =
+                (request.type === "process") ? this.processRaw(request.content) : this.processRequest(request);
 
             const response: Response = JSON.parse(
-                this.server.Pointer_stringify(responseJsonPtr));
+                this.server.Pointer_stringify(responseJson));
+            this.freeJson(responseJson);
 
-            this.freeJson(responseJsonPtr);
-
-            if (!response.success) {
-                reject(response.errorText);
-            } else {
+            if (response.success) {
                 resolve(response);
+            } else {
+                reject(response.errorText);
             }
         });
     }
 
-    private processRaw(request: ProcessRequest): Promise<Response> {
-        return new Promise<Response>((resolve, reject) => {
+    private processRequest(request: Request): ResponsePointer {
+        const requestJson: string = JSON.stringify(request);
+        const requestJsonPtr: number = this.server.allocate(
+            this.server.intArrayFromString(requestJson), "i8",
+            Allocator.ALLOC_NORMAL);
 
-            const nchannels = request.processInput.inputBuffers.length;
-            const nframes = request.processInput.inputBuffers[0].values.length;
+        const responseJsonPtr: number = this.doRequest(requestJsonPtr);
+        this.server._free(requestJsonPtr);
+        return responseJsonPtr;
+    }
 
-            const bufsPtr = this.server._malloc(nchannels * 4);
-            const bufs = new Uint32Array(
-                this.server.HEAPU8.buffer, bufsPtr, nchannels);
+    private processRaw(request: ProcessRequest): ResponsePointer {
+        const nchannels = request.processInput.inputBuffers.length;
+        const nframes = request.processInput.inputBuffers[0].values.length;
 
-            for (let i = 0; i < nchannels; ++i) {
-                const framesPtr = this.server._malloc(nframes * 4);
-                const frames = new Float32Array(
-                    this.server.HEAPU8.buffer, framesPtr, nframes);
-                frames.set(request.processInput.inputBuffers[i].values);
-                bufs[i] = framesPtr;
-            }
+        const bufsPtr = this.server._malloc(nchannels * 4);
+        const bufs = new Uint32Array(
+            this.server.HEAPU8.buffer, bufsPtr, nchannels);
 
-            const responseJsonPtr: number = this.doProcess(
-                request.pluginHandle,
-                bufsPtr,
-                request.processInput.timestamp.s,
-                request.processInput.timestamp.n);
+        for (let i = 0; i < nchannels; ++i) {
+            const framesPtr = this.server._malloc(nframes * 4);
+            const frames = new Float32Array(
+                this.server.HEAPU8.buffer, framesPtr, nframes);
+            frames.set(request.processInput.inputBuffers[i].values);
+            bufs[i] = framesPtr;
+        }
 
-            for (let i = 0; i < nchannels; ++i) {
-                this.server._free(bufs[i]);
-            }
-            this.server._free(bufsPtr);
+        const responseJsonPtr: number = this.doProcess(
+            request.pluginHandle,
+            bufsPtr,
+            request.processInput.timestamp.s,
+            request.processInput.timestamp.n);
 
-            const response: Response = JSON.parse(
-                this.server.Pointer_stringify(responseJsonPtr));
-            this.freeJson(responseJsonPtr);
+        for (let i = 0; i < nchannels; ++i) {
+            this.server._free(bufs[i]);
+        }
+        this.server._free(bufsPtr);
 
-            if (!response.success) {
-                reject(response.errorText);
-            } else {
-                resolve(response);
-            }
-        });
+        return responseJsonPtr;
     }
 
 }
