698fa4e5e337d87bc810929ceb3a560752c32877
Lucas Thompson <dev@lucas.im>
2016-09-28 17:36:44 +0100

    Export SampleType making it available as part of the install library.

--- src/feats.ts
+++ src/feats.ts
@@ -3,6 +3,6 @@
  */
 
 export {FeatsModuleClient} from "./FeatsModuleClient";
-export {AdapterFlags} from "./FeatureExtractor";
+export {AdapterFlags, SampleType} from "./FeatureExtractor";
 export {LocalModuleRequestHandler} from "./LocalModuleRequestHandler";
 export {batchProcess, generateSineWave, segmentAudioBuffer} from "./AudioUtilities";
