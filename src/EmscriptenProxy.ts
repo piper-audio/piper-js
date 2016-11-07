a7ae75ac13cc976c02306b9d711a2f391f77a68c
Lucas Thompson <dev@lucas.im>
2016-11-07 15:30:31 +0000

    Introduce some namespacing for Json (de)serialisation filters, and also now expose the underlying functions for handling strings. Also extract out the Emscripten Json request and raw process requests calls for use elsewhere.

--- src/EmscriptenProxy.ts
+++ src/EmscriptenProxy.ts
@@ -9,11 +9,7 @@ import {
     FinishResponse, FinishRequest, compose
 } from "./Piper";
 import {
-    deserialiseJsonListResponse,
-    serialiseJsonListRequest, deserialiseJsonLoadResponse,
-    serialiseJsonLoadRequest, deserialiseJsonConfigurationResponse,
-    serialiseJsonConfigurationRequest, deserialiseJsonProcessResponse,
-    deserialiseJsonFinishResponse, serialiseJsonFinishRequest
+    Filters
 } from "./JsonProtocol";
 
 export class EmscriptenProxy implements Service {
@@ -25,39 +21,39 @@ export class EmscriptenProxy implements Service {
 
     list(request: ListRequest): Promise<ListResponse> {
         return compose(
-            deserialiseJsonListResponse, compose(
-                serialiseJsonListRequest, emscriptenService(this.module)
+            Filters.deserialiseJsonListResponse, compose(
+                Filters.serialiseJsonListRequest, emscriptenService(this.module)
             )
         )(request);
     }
 
     load(request: LoadRequest): Promise<LoadResponse> {
         return compose(
-            deserialiseJsonLoadResponse, compose(
-                serialiseJsonLoadRequest, emscriptenService(this.module)
+            Filters.deserialiseJsonLoadResponse, compose(
+                Filters.serialiseJsonLoadRequest, emscriptenService(this.module)
             )
         )(request);
     }
 
     configure(request: ConfigurationRequest): Promise<ConfigurationResponse> {
         return compose(
-            deserialiseJsonConfigurationResponse, compose(
-                serialiseJsonConfigurationRequest, emscriptenService(this.module)
+            Filters.deserialiseJsonConfigurationResponse, compose(
+                Filters.serialiseJsonConfigurationRequest, emscriptenService(this.module)
             )
         )(request);
     }
 
     process(request: ProcessRequest): Promise<ProcessResponse> {
         return compose(
-            deserialiseJsonProcessResponse,
+            Filters.deserialiseJsonProcessResponse,
             emscriptenProcess(this.module)
         )(request);
     }
 
     finish(request: FinishRequest): Promise<FinishResponse> {
         return compose(
-            deserialiseJsonFinishResponse, compose(
-                serialiseJsonFinishRequest, emscriptenService(this.module)
+            Filters.deserialiseJsonFinishResponse, compose(
+                Filters.serialiseJsonFinishRequest, emscriptenService(this.module)
             )
         )(request);
     }
@@ -72,26 +68,68 @@ const freeJson = (emscripten: EmscriptenModule, ptr: Pointer): void => emscripte
 
 type Pointer = number;
 
+function jsonRequest(emscripten: EmscriptenModule, request: string): string {
+    const doRequest = emscripten.cwrap(
+        "piperRequestJson",
+        "number",
+        ["number"]
+    ) as (ptr: number) => number;
+
+    const requestJson: Pointer = emscripten.allocate(
+        emscripten.intArrayFromString(request), "i8",
+        Allocator.ALLOC_NORMAL);
+
+    const responseJson: Pointer = doRequest(requestJson);
+    emscripten._free(requestJson);
+
+    const jsonString: string = emscripten.Pointer_stringify(responseJson);
+    freeJson(emscripten, responseJson);
+    return jsonString;
+}
+
+function rawProcess(emscripten: EmscriptenModule, request: ProcessRequest): string {
+    const doProcess = emscripten.cwrap(
+        "piperProcessRaw",
+        "number",
+        ["number", "number", "number", "number"]
+    ) as (handle: number, bufs: number, sec: number, nsec: number) => number;
+
+    const nChannels: number = request.processInput.inputBuffers.length;
+    const nFrames: number = request.processInput.inputBuffers[0].length;
+    const buffersPtr: Pointer = emscripten._malloc(nChannels * 4);
+    const buffers: Uint32Array = new Uint32Array(
+        emscripten.HEAPU8.buffer, buffersPtr, nChannels);
+
+    for (let i = 0; i < nChannels; ++i) {
+        const framesPtr: Pointer = emscripten._malloc(nFrames * 4);
+        const frames: Float32Array = new Float32Array(
+            emscripten.HEAPU8.buffer, framesPtr, nFrames);
+        frames.set(request.processInput.inputBuffers[i]);
+        buffers[i] = framesPtr;
+    }
+
+    const responseJson: Pointer = doProcess(
+        request.handle,
+        buffersPtr,
+        request.processInput.timestamp.s,
+        request.processInput.timestamp.n
+    );
+
+    for (let i = 0; i < nChannels; ++i) {
+        emscripten._free(buffers[i]);
+    }
+
+    emscripten._free(buffersPtr);
+
+    const jsonString: string = emscripten.Pointer_stringify(responseJson);
+    freeJson(emscripten, responseJson);
+    return jsonString;
+}
+
 function emscriptenService(emscripten: EmscriptenModule)
 : ServiceFunc<string, string> {
     return (request: string): Promise<string> => {
-
-        const doRequest = emscripten.cwrap(
-            "piperRequestJson",
-            "number",
-            ["number"]
-        ) as (ptr: number) => number;
-
-        const requestJson: Pointer = emscripten.allocate(
-            emscripten.intArrayFromString(request), "i8",
-            Allocator.ALLOC_NORMAL);
-
-        const responseJson: Pointer = doRequest(requestJson);
-        emscripten._free(requestJson);
-
-        const jsonString: string = emscripten.Pointer_stringify(responseJson);
-        freeJson(emscripten, responseJson);
-        return Promise.resolve(jsonString);
+        return Promise.resolve(jsonRequest(emscripten, request));
     }
 }
 
@@ -99,41 +137,5 @@ const emscriptenProcess
     : (emscripten: EmscriptenModule) => ServiceFunc<ProcessRequest, string>
     = (emscripten: EmscriptenModule) =>
     (request: ProcessRequest): Promise<string> => {
-
-        const doProcess = emscripten.cwrap(
-            "piperProcessRaw",
-            "number",
-            ["number", "number", "number", "number"]
-        ) as (handle: number, bufs: number, sec: number, nsec: number) => number;
-
-        const nChannels: number = request.processInput.inputBuffers.length;
-        const nFrames: number = request.processInput.inputBuffers[0].length;
-        const buffersPtr: Pointer = emscripten._malloc(nChannels * 4);
-        const buffers: Uint32Array = new Uint32Array(
-            emscripten.HEAPU8.buffer, buffersPtr, nChannels);
-
-        for (let i = 0; i < nChannels; ++i) {
-            const framesPtr: Pointer = emscripten._malloc(nFrames * 4);
-            const frames: Float32Array = new Float32Array(
-                emscripten.HEAPU8.buffer, framesPtr, nFrames);
-            frames.set(request.processInput.inputBuffers[i]);
-            buffers[i] = framesPtr;
-        }
-
-        const responseJson: Pointer = doProcess(
-            request.handle,
-            buffersPtr,
-            request.processInput.timestamp.s,
-            request.processInput.timestamp.n
-        );
-
-        for (let i = 0; i < nChannels; ++i) {
-            emscripten._free(buffers[i]);
-        }
-
-        emscripten._free(buffersPtr);
-
-        const jsonString: string = emscripten.Pointer_stringify(responseJson);
-        freeJson(emscripten, responseJson);
-        return Promise.resolve(jsonString);
-    };
+        return Promise.resolve(rawProcess(emscripten, request));
+};
