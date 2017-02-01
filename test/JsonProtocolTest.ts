566fb3f743d53e13030cc08db30a4c399db5fdc4
Lucas Thompson <dev@lucas.im>
2017-02-01 15:37:08 +0000

    Add a test case verifying the serialisation of JsonResponses with featureValues, and also exercising the toBase64 flag.

--- test/JsonProtocolTest.ts
+++ test/JsonProtocolTest.ts
@@ -69,4 +69,57 @@ describe("Serialise.ProcessResponse()", () => {
         };
         JSON.parse(Serialise.ProcessResponse(toSerialise)).should.eql(expected);
     });
+
+    it("can serialise ProcessResponses with featureValues", () => {
+       const toSerialise: ProcessResponse = {
+           handle: 1,
+           features: new Map<string, FeatureList>([
+               ["values-n-stamps", [
+                   {
+                       timestamp: {s: 0, n: 0},
+                       featureValues: new Float32Array([0, 0, 0, 0])
+                   },
+                   {
+                       timestamp: {s: 0, n: 500000000},
+                       featureValues: new Float32Array([1, 1, 1, 1])
+                   },
+                   {
+                       timestamp: {s: 1, n: 0},
+                       featureValues: new Float32Array([2, 2, 2, 2])
+                   },
+                   {
+                       timestamp: {s: 1, n: 500000000},
+                       featureValues: new Float32Array([3, 3, 3, 3])
+                   }
+               ]]
+           ])
+       };
+       const expected: any = {
+           method: "process",
+           result: {
+               handle: 1,
+               features: {
+                   "values-n-stamps": [
+                       {
+                           timestamp: {s: 0, n: 0},
+                           featureValues: [0, 0, 0, 0]
+                       },
+                       {
+                           timestamp: {s: 0, n: 500000000},
+                           featureValues: [1, 1, 1, 1]
+                       },
+                       {
+                           timestamp: {s: 1, n: 0},
+                           featureValues: [2, 2, 2, 2]
+                       },
+                       {
+                           timestamp: {s: 1, n: 500000000},
+                           featureValues: [3, 3, 3, 3]
+                       }
+                   ]
+               }
+           }
+       };
+       JSON.parse(Serialise.ProcessResponse(toSerialise, false)).should.eql(expected);
+    });
 });
