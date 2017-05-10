b87ac08bd78f7a102c6cdb50a681a320478fbbbb
Chris Cannam <cannam@all-day-breakfast.com>
2017-05-10 09:52:03 +0100

    Introduce a collect test

--- test/VampTestPluginTest.ts
+++ test/VampTestPluginTest.ts
@@ -7,26 +7,80 @@ import VampTestPlugin from '../ext/VampTestPluginModule';
 import {AdapterFlags} from "../src/FeatureExtractor";
 import {LoadResponse, LoadRequest} from "../src/Piper";
 
+//!!! todo review which of these are actually necessary
+import {
+    processConfiguredExtractor,
+    segment,
+    process,
+    collect,
+    AudioStreamFormat,
+    AudioData,
+    CreateFeatureExtractorFunction,
+    CreateAudioStreamFunction, PiperSimpleClient, FeatureCollection,
+    SimpleRequest,
+    VectorFeatures, MatrixFeatures, TrackFeatures
+} from "../src/HigherLevelUtilities";
+
+var expect = chai.expect;
 chai.should();
 chai.use(chaiAsPromised);
 
 describe('VampTestPlugin', () => {
-    const client = new PiperVampService(VampTestPlugin());
+
+    const rate = 44100;
+    const blockSize = 1024;
+    const eps = 1e-6;
+
+    const key = "vamp-test-plugin:vamp-test-plugin";
+    const keyFreq = "vamp-test-plugin:vamp-test-plugin-freq";
+
+    const inputData = ((n : number) => {
+        const buf = new Float32Array(n);
+        // start at 1, not 0 so that all elts are non-zero
+        for (let i = 0; i < n; ++i) buf[i] = i + 1;
+        return buf;
+    });
+    
+    const service = new PiperVampService(VampTestPlugin());
 
     const loadResponse: Promise<LoadResponse> =
-        client.list({}).then((resp) => {
-            return client.load({
+        service.list({}).then((resp) => {
+            return service.load({
                 key: resp.available[0].key, // time-domain
                 inputSampleRate: 44100,
                 adapterFlags: [AdapterFlags.AdaptAllSafe]
             } as LoadRequest);
         });
 
-    it('Can load test plugin', () => {
-        // yuk
+    it("can load test plugin using service", () => {
         loadResponse.then(resp => {
-            resp.handle.should.equal(1);
+            expect(resp.handle).to.be.a('number');
         })
+    });
+
+    const client = new PiperSimpleClient(service);
+
+    const makeRequest = ((n: number, output: string) => {
+        const buf = inputData(n);
+        const request : SimpleRequest = {
+            audioData: [buf],
+            audioFormat: {
+                channelCount: 1,
+                sampleRate: rate
+            },
+            key: key,
+            outputId: output,
+            blockSize,
+            stepSize: blockSize
+        };
+        return request;
+    });
+    
+    it("can collect output using simple client", () => {
+        const request = makeRequest(blockSize * 10, "input-timestamp");
+        client.collect(request).then(response => {
+            expect(response.features).to.exist;
+        });
     });
 
 });
