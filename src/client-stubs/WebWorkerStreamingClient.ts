0896ae4055b348dd1850e33648b0e5a9633a3e81
Lucas Thompson <dev@lucas.im>
2017-06-19 14:26:30 +0100

    Tweak observable logic to test id against queued / running requests - allowing for multiple requests to be sent from the client, and matching up of their responses.

--- src/client-stubs/WebWorkerStreamingClient.ts
+++ src/client-stubs/WebWorkerStreamingClient.ts
@@ -26,11 +26,13 @@ export class WebWorkerStreamingClient implements StreamingService {
     private worker: Worker;
     private idProvider: RequestIdProvider;
     private messages$: ResponseObservable;
+    private running: RequestId[];
 
     constructor(worker: Worker, idProvider: RequestIdProvider) {
         this.worker = worker;
         this.idProvider = idProvider;
-        this.messages$ = Observable.fromEvent(this.worker, "message");
+        this.messages$ = Observable.fromEvent(this.worker, "message").share();
+        this.running = [];
     }
 
     list(request: ListRequest): Promise<ListResponse> {
@@ -42,8 +44,7 @@ export class WebWorkerStreamingClient implements StreamingService {
                     id: id,
                     method: method,
                     params: request
-                },
-                method
+                }
             )
             .filter(res => (res. data as SuccessfulResponse<ListResponse>)
                 .result.available !== undefined) /* TODO should this throw?*/
@@ -54,20 +55,18 @@ export class WebWorkerStreamingClient implements StreamingService {
     }
 
     process(request: SimpleRequest): Observable<StreamingResponse> {
-        return this.createFeatureStream(request, "process");
+        return this.createFeatureStream(request);
     }
 
-    private createFeatureStream(request: SimpleRequest,
-                                method: WebMethod): Observable<StreamingResponse> {
+    private createFeatureStream(request: SimpleRequest)
+    : Observable<StreamingResponse> {
         const id: RequestId = this.idProvider.next().value;
 
         return this.createThrowingObserver<SimpleRequest>({
                 id: id,
-                method: method,
+                method: "process",
                 params: request
-            },
-            method,
-            "finish"
+            }
         )
         /*TODO inspect res.data to ensure valid StreamingResponse?*/
             .takeWhile(val => val.data.method !== "finish")
@@ -77,31 +76,34 @@ export class WebWorkerStreamingClient implements StreamingService {
     }
 
     // TODO take predicate and also check response validity
-    private createThrowingObserver<T>(seedRequest: RequestMessage<T>,
-                                   ...method: WebMethod[]): ResponseObservable {
+    private createThrowingObserver<T>(seedRequest: RequestMessage<T>): ResponseObservable {
         const sendRequest$ = Observable.create(() => {
+            this.running.push(seedRequest.id);
             this.worker.postMessage(seedRequest);
+            return () => {
+                const i = this.running.findIndex(id => id === seedRequest.id);
+                if (i !== -1) {
+                    this.running.splice(i, 1);
+                }
+            };
         });
 
-        // TODO this won't work if multiple requests are being processed
-        // perhaps errors shouldn't be thrown, and each observer should just
-        // filter for matching id, allowing the other messages to pass through.
-        // Then, it still may make sense to throw if the method doesn't match
         return this.messages$
             .merge(sendRequest$)
             .do((val: ResponseMessage<any>) => {
                 // TODO piper specific exception types
-                if (val.data.id !== seedRequest.id) {
-                    throw new Error("Wrong response id");
+                if (!this.running.includes(val.data.id)) {
+                    throw new Error("Invalid response id");
                 }
-                if (!method.includes(val.data.method)) {
-                    throw new Error("Wrong response type");
+                if (!["list", "process", "finish"].includes(val.data.method)) {
+                    throw new Error("Invalid response type");
                 }
                 if (this.isErrorResponse(val.data)) {
                     const error = val.data.error;
                     throw new Error(`${error.code}: ${error.message}`);
                 }
-            });
+            })
+            .filter(val => val.data.id === seedRequest.id);
     }
 
     private isErrorResponse<T>(data: ResponseData<T>): data is ErrorResponse {
