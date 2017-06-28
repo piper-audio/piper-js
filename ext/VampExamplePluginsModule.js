a69344102751e567ec1bcf86046071ddbc5f9833
Lucas Thompson <dev@lucas.im>
2017-06-28 09:39:35 +0100

    Update builds

--- ext/VampExamplePluginsModule.js
+++ ext/VampExamplePluginsModule.js
@@ -2,4 +2,4 @@
  * Created by lucas on 02/12/2016.
  */
 module.exports.VampExamplePlugins = require("./VampExamplePlugins.umd");
-module.exports.default = module.exports.VampExamplePlugins.createLibrary;
+module.exports.default = module.exports.VampExamplePlugins;
--- ext/VampTestPluginModule.js
+++ ext/VampTestPluginModule.js
@@ -2,4 +2,4 @@
  * Created by lucas on 02/12/2016.
  */
 module.exports.VampTestPlugin = require("./VampTestPlugin.umd");
-module.exports.default = module.exports.VampTestPlugin.createLibrary;
+module.exports.default = module.exports.VampTestPlugin;
