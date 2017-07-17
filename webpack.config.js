c8aaea86f978e4f8fb70076249e8f0681e96e1dd
Lucas Thompson <dev@lucas.im>
2017-07-17 16:56:52 +0100

    Basic webpack config for a umd module

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+/**
+ * Created by lucast on 17/07/2017.
+ */
+const path = require('path');
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, "dist"), // string
+        filename: 'piper.bundle.js',
+        library: "Piper",
+        libraryTarget: "umd"
+    }
+};
