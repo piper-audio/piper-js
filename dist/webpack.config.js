a64bc59967bf3a46b83c27c81a6eece331ac6243
Lucas Thompson <dev@lucas.im>
2017-07-18 15:14:52 +0100

    No longer move ts build artifacts to root. Trying to figure out what is needed to consume the library in TypeScript.

--- dist/webpack.config.js
+++ dist/webpack.config.js
@@ -1,11 +1,10 @@
 /**
  * Created by lucast on 17/07/2017.
  */
-const path = require('path');
 module.exports = {
     entry: './dist/ts/index.js',
     output: {
-        path: path.resolve(__dirname, "dist"), // string
+        path: __dirname,
         filename: 'piper.bundle.js',
         library: "Piper",
         libraryTarget: "umd"
