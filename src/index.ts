389405729d825512e12c6158d5a48a7d24e730c8
Lucas Thompson <dev@lucas.im>
2017-07-19 16:34:57 +0100

    Basic export, without interface definitions etc. I think for TypeScript I may return to importing individual modules.

--- src/index.ts
+++ src/index.ts
@@ -4,72 +4,26 @@
 // main barrel, import library functionality and export in desired shape
 
 // exports for library consumption
-import {Client, FeatureExtractorSynchronousService} from './core';
-import * as timestamp from './time';
-import * as fftUtils from './fft';
-import {
-    EmscriptenService,
-    EmscriptenSynchronousService,
-    EmscriptenFeatureExtractor
-} from './emscripten';
-import {
-    FeatureExtractorService
-} from './core';
-import { OneShotExtractionClient } from './one-shot';
-import * as streamingStuff from './streaming';
-import * as ZeroCrossings from './extractors/zero-crossings';
-import * as webWorkerClientStuff from './clients/web-worker-streaming';
-import {WebWorkerStreamingServer} from './servers/web-worker-streaming';
-import * as extractorStuff from './FeatureExtractor';
-import * as piperStuff from './core';
-import * as workerProtocol from './protocols/web-worker';
-import * as featureStuff from './Feature';
-import {RealFft as IRealFft} from './fft';
-import {
-    StreamingProgress,
-    StreamingConfiguration,
-    StreamingResponse,
-    StreamingService,
-    ProgressCallback
-} from './streaming';
-import {Timestamp} from './time';
+import * as core from './core';
+import * as fft from './fft';
+import * as audio from './audio';
+import * as emscripten from './emscripten';
+import * as oneShot from './one-shot';
+import * as streaming from './streaming';
+import * as time from './time';
+import ZeroCrossings from './extractors/zero-crossings';
+import * as webWorker from './web-worker';
 
-const core = Object.assign({
-    PiperClient: Client,
-    PiperSimpleClient: OneShotExtractionClient,
-    timestamp,
-    fft: Object.assign({}, fftUtils)
-}, piperStuff, featureStuff);
-
-// TypeScript merges this with the above
-namespace core {
-    export namespace fft {
-        // export interface RealFft extends IRealFft {}
-    }
-}
-
-const vamp = {
-    PiperVampService: EmscriptenService,
-    PiperVampSynchronousService: EmscriptenSynchronousService,
-    PiperVampFeatureExtractor: EmscriptenFeatureExtractor,
-};
-
-const streaming = streamingStuff;
-
-const worker = Object.assign(
-    {WebWorkerStreamingServer},
-    webWorkerClientStuff,
-    workerProtocol
-);
-
-const extractor = Object.assign(
-    {
-        FeatureExtractorSynchronousService,
-        FeatureExtractorService
+export = {
+    core,
+    fft,
+    audio,
+    emscripten,
+    oneShot,
+    streaming,
+    time,
+    extractors: {
+        ZeroCrossings
     },
-    extractorStuff
-);
-
-const extractors = {
-    ZeroCrossings
+    webWorker
 };
