f6dfe743b775de70e318b1ce6c91ba92023f6ea5
Lucas Thompson <dev@lucas.im>
2017-08-12 22:55:12 +0100

    Install newer UglifyPlugin for doing ugly stuff to ES6

--- dist/webpack.config.js
+++ dist/webpack.config.js
@@ -1,13 +1,22 @@
 /**
  * Created by lucast on 17/07/2017.
  */
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 module.exports = {
-    entry: './dist/ts/index.js',
-    output: {
-        path: __dirname,
-        filename: 'piper.bundle.js',
-        library: "Piper",
-        libraryTarget: "umd"
-    },
-    devtool: 'source-map'
+  entry: './dist/ts/index.js',
+  output: {
+    path: __dirname,
+    filename: 'piper.bundle.js',
+    library: "Piper",
+    libraryTarget: "umd"
+  },
+  devtool: 'source-map',
+  plugins: [
+    new UglifyJSPlugin(
+      {
+        sourceMap: true,
+        uglifyOptions: { mangle: false } // seems there is a bug otherwise
+      }
+    )
+  ]
 };
